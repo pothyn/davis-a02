9fffa6a92bfa0caca3eb52504ea4b39f3d6d4596
pothyn <pothyne@outlook.com>
2021-09-16 16:46:36 -0400

    Solution08 commit

---
+++ exercise08/src/main/java/Solution08.java
@@ -0,0 +1,23 @@
+public class Solution08 {
+
+    public static void main(String[] args) {
+//        How many people? 8
+//        How many pizzas do you have? 2
+//        How many slices per pizza? 8
+//        8 people with 2 pizzas (16 slices)
+//        Each person gets 2 pieces of pizza.
+//        There are 0 leftover pieces.
+
+//        Ask how many people and store in peopleCount
+//        Ask how many pizzas and store in pizzaCount
+//        Ask how many slices per pizza and store in slicesPerPizza
+//        Compute totalSlices = slicesPerPizza * pizzaCount
+//        Compute slicesPerPerson = totalSlices / peopleCount
+//        Compute  leftoverSlices = totalSlices % peopleCount
+//        Print slicesPerPerson
+//        Print leftoverSlices
+
+
+    }
+
+}
