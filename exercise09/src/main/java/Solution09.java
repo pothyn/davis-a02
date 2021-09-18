8bc97aed4a0099418408b0bdd410e8891f0effbd
pothyn <pothyne@outlook.com>
2021-09-17 22:26:06 -0400

    Fixed copyright statement

--- exercise09/src/main/java/Solution09.java
+++ exercise09/src/main/java/Solution09.java
@@ -1,6 +1,6 @@
 /*
- *  UCF COP3330 Fall 2021 Assignment 2 Solution
- *  Copyright 2021 first_name last_name
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 09
+ *  Copyright 2021 Hunter Davis
  */
 
 import java.util.Scanner;
